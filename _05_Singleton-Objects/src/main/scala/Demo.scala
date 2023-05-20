7f7b2bae5081dc6de267cdf6b80a5e24e2ec3617
emote2614 <emote2614@gmail.com>
2023-05-20 19:41:32 +0900

    Singleton_Objects

--- _05_Singleton-Objects/src/main/scala/Demo.scala
+++ _05_Singleton-Objects/src/main/scala/Demo.scala
@@ -2,31 +2,41 @@
 object Demo {
   def main(args: Array[String]): Unit = {
 //    === "Utility" methods ==============================
-    object StringUtils{
-      // 문자열이 비어있거나 null인지 판단
-      def isNullOrEmpty(s:String):Boolean = {s == null || s.trim.isEmpty}
-      // 문자열의 왼쪽에 있는 모든 공백제거
-      def leftTrim(s:String):String = s.replaceAll("^\\s+", "")
-      // 문자열의 오른쪽에 있는 모든 공백제거
-      def rightTrim(s:String):String = s.replaceAll("\\s+$", "")
-    }
-    val x = StringUtils.isNullOrEmpty("")
-    println(x)
-    val y = StringUtils.isNullOrEmpty("a")
-    println(y)
+//    object StringUtils{
+//      // 문자열이 비어있거나 null인지 판단
+//      def isNullOrEmpty(s:String):Boolean = {s == null || s.trim.isEmpty}
+//      // 문자열의 왼쪽에 있는 모든 공백제거
+//      def leftTrim(s:String):String = s.replaceAll("^\\s+", "")
+//      // 문자열의 오른쪽에 있는 모든 공백제거
+//      def rightTrim(s:String):String = s.replaceAll("\\s+$", "")
+//    }
+//    val x = StringUtils.isNullOrEmpty("")
+//    println(x)
+//    val y = StringUtils.isNullOrEmpty("a")
+//    println(y)
 //    === Companion objects ==============================
-    class Circle(radius: Double) {
-      import Circle._
-      def area: Double = calculateArea(radius)
-    }
-    object Circle {
-      private def calculateArea(radius: Double): Double =
-        math.Pi * math.pow(radius, 2.0)
-    }
-    val circle1 = new Circle(5.0)
-    println(circle1.area)
+//    class Circle(radius: Double) {
+//      import Circle._
+//      def area: Double = calculateArea(radius)
+//    }
+//    object Circle {
+//      private def calculateArea(radius: Double): Double =
+//        math.Pi * math.pow(radius, 2.0)
+//    }
+//    val circle1 = new Circle(5.0)
+//    println(circle1.area)
 //    === Creating modules from traits ==============================
+    trait AddService{
+      def add(a:Int, b:Int) = a + b
+    }
+    trait MultiplyService{
+      def multiply(a:Int, b:Int) = a * b
+    }
+    object MathService extends AddService with MultiplyService
 
+    import MathService._
+    println(add(1, 1))
+    println(multiply(2, 2))
 
   }
 }
