b6d7950581cf3660fd743b2dd4921f88b8ee31f2
emote2614 <emote2614@gmail.com>
2023-05-25 23:53:40 +0900

    .

--- _25_set-class/src/main/scala/Demo.scala
+++ _25_set-class/src/main/scala/Demo.scala
@@ -7,5 +7,18 @@ object Demo {
     set ++= Vector(4, 5)
     println(set)
 
+    set -= 1
+    set -= 2 -= 3
+    set --= Vector(4, 5)
+    println(set)
+
+    val set2 = scala.collection.mutable.Set[Int](1, 1, 2, 2, 3, 3, 3, 4)
+    println(set2)
+    set2.remove(2)
+
+
+
+
+
   }
 }
